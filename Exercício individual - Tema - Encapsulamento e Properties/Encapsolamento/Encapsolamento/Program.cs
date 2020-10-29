6ff874118800a9eae1aeecba8d1e8bed6f91d2b2
GabrielGarciaRibeiroDeSouza <gabriel.garcia.ribeiro@gmail.com>
2020-10-29 15:38:10 -0300

    Encapsolamento e properties

---
+++ Exercício individual - Tema - Encapsulamento e Properties/Encapsolamento/Encapsolamento/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsolamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicializa o objeto com um valor
+            Produto p = new Produto("ps4", 6000, 10);
+
+            //faz os printes de cada propriedade do produto usando o properties
+            Console.WriteLine("Nome: " + p.Nome);
+            Console.WriteLine("Preço: R$" + p.Preco);
+            Console.WriteLine("Quantidade: " + p.Quantidade);
+
+        }
+    }
+}
