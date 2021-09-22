4fc8aef6b140fc5edb5f21217419996b7130214b
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2021-09-22 20:50:17 -0300

    Exercicio do calculo de circulo Exemplo1

---
+++ Calculo_do_Circulo/Calculo_do_Circulo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculo_do_Circulo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //declara a variavel calc para instanciar a classe calculadora.
+            calculadora calc = new calculadora();
+            
+            //imprime uma mesagem no console e depois espera receber um valor do usuario
+            Console.WriteLine("Entre com valor do raio:");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            //declaracao daa variaveis circ e volume, que pega a funcao da classe calculadora. 
+            double circ = calc.Circuferencia(raio);
+            double volume = calc.Volume(raio);
+            
+            //imprime os valores em sequecia das variaveis circ, volume e pi (que fica dentro da classe calculadora)
+            Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " +calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        
+    }
+}
+
---
+++ Calculo_do_Circulo/Calculo_do_Circulo/calculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculo_do_Circulo
+{
+    class calculadora
+    {
+        //declaraçao da variavel pi;
+        public double Pi = 3.14;
+        
+        //declaraçao da funcao circunferencia com o parametro de double r.
+        public double Circuferencia(double r)
+        {
+            //retorna o resultado da conta abaixo.
+            return 2.0 * Pi * r;
+        }
+
+        //declaraçao da funcao volume com parametro de double r.
+        public double Volume(double r)
+        {
+            //retorna o resultado da conta abaixo.
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
