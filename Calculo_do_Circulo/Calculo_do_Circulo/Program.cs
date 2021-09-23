579a88e93fbacf5d449f36a19440545ce7609df5
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2021-09-22 22:06:50 -0300

    Exemplo 2 com static

--- Calculo_do_Circulo/Calculo_do_Circulo/Program.cs
+++ Calculo_do_Circulo/Calculo_do_Circulo/Program.cs
@@ -5,25 +5,21 @@ namespace Calculo_do_Circulo
 {
     class Program
     {
-        
+        //calculo do circulo usando o static.
         static void Main(string[] args)
         {
-            //GG_non
-            //declara a variavel calc para instanciar a classe calculadora.
-            calculadora calc = new calculadora();
-            
             //imprime uma mesagem no console e depois espera receber um valor do usuario
             Console.WriteLine("Entre com valor do raio:");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             
             //declaracao daa variaveis circ e volume, que pega a funcao da classe calculadora. 
-            double circ = calc.Circuferencia(raio);
-            double volume = calc.Volume(raio);
+            double circ = Calculadora.Circuferencia(raio);
+            double volume = Calculadora.Volume(raio);
             
             //imprime os valores em sequecia das variaveis circ, volume e pi (que fica dentro da classe calculadora)
             Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " +calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
         
     }
