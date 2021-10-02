756f459e7186188aeb03e5e4aa6b124391d6e4ac
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2021-10-01 21:36:47 -0300

    Controle de estoque

---
+++ Exer-Controle de Estoque/ControleEstoque/ControleEstoque/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleEstoque
+{
+    class Produto
+    {
+        //Delcaração de variaveis
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //cria o construtor com 3 paramaetros, sendo eles os mesmos nomes das variaveis só que em minusculo
+        public Produto ( string nome, double preco, int quantidada)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidada;
+        }
+
+        //função para calculo do valor em estoque
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //função para adicionar produtos ao estoque
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        // função para remover produtos do estoque
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        //função para mostrar o nome, quantidade e preco do estoque atual.
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade 
+            + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exer-Controle de Estoque/ControleEstoque/ControleEstoque/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*esse bloco recebe os dados do usuario. Vale ressaltar que os "=" e "_" são para deixar a
+             visulização no console mais organizada. */
+            Console.WriteLine("===================================");
+            Console.WriteLine("Entre com os dados do produto;");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            //instancia o objeto já com os dados do usuario
+            Produto p = new Produto(nome, preco, quantidade);
+
+            //mostra os dados atuais para o usuario
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine("________________________________");
+           
+            /*nesse bloco ele pede para o usuario digitar a quantidade de produtos a ser adicionado e
+             em seguida mostra os dados atuais do produto. */
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+            Console.WriteLine("Dados do atualizados: " + p);
+            Console.WriteLine("________________________________");
+
+            /*nesse bloco pede a quantidada de produtos a serem removidos do estoque e em seguida
+             mostra novamento os dados atuais do estoque. */
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+            Console.WriteLine("Dados do atualizados: " + p);
+            Console.WriteLine("===================================");
+        }
+    }
+}
