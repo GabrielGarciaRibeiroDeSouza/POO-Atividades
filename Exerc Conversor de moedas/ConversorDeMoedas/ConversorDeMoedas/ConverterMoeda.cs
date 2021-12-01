d14d1193f639383e63d4a48f6a81c2c3188e5670
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2021-12-01 18:57:54 -0300

    Tema: Conversor de moedas

---
+++ Exerc Conversor de moedas/ConversorDeMoedas/ConversorDeMoedas/ConverterMoeda.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConversorDeMoedas
+{
+    class ConverterMoeda
+    {
+        //declara variaveis com autoproperties
+        public double Dolar { get; private set; }
+        public double CompraDolar { get; private set; }
+        public double IOF { get; private set; }
+        
+
+        //variaveis privadas
+        private double dinheiroBruto;
+        private double total;
+
+        //construtor com 2 parametros
+        public ConverterMoeda(double dolar, double compraDolar)
+        {
+            Dolar = dolar;
+            CompraDolar = compraDolar;
+            IOF = 2.95;
+        }
+
+        //função para fazer o calculo da converção
+        public void ConversorMoeda()
+        {
+            dinheiroBruto = Dolar * CompraDolar;
+            total = (IOF/100) * dinheiroBruto + dinheiroBruto;
+        }
+
+        public override string ToString()
+        {
+            //mostra o resultado final para o usuario
+            return "Cotação do Dolar: "
+                + "R$" + Dolar.ToString("F2", CultureInfo.InvariantCulture)
+
+                + "\nQuantos dolar você irá comprar? "
+                + "R$" + CompraDolar.ToString("F1", CultureInfo.InvariantCulture)
+
+                + "\nValor a ser pago em Reais: "
+                + "R$" + dinheiroBruto.ToString("F2", CultureInfo.InvariantCulture)
+
+                + "\nValor do IOF: "
+                + IOF.ToString("F2", CultureInfo.InvariantCulture) + "%"
+
+                + "\nValor Total da compra com IOF: "
+                + "R$" + total.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exerc Conversor de moedas/ConversorDeMoedas/ConversorDeMoedas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //nesse bloco o usuario insere os dados solicitados
+            Console.WriteLine("Informe a cotação do dolar: ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade de dolar que vai comprar: ");
+            double comprarDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //cria um objeto e usa as variaveis com os dados digitados pelo usuario
+            ConverterMoeda conversor = new ConverterMoeda(dolar, comprarDolar);
+            
+            //usa a função para converter as moedas
+            conversor.ConversorMoeda();
+
+            //imprime o resultado
+            Console.WriteLine("=======================");
+            Console.WriteLine(conversor.ToString());
+
+            /*
+             o projeto foi criado com base em uma uml simples que criei, depois utilizei os codigos
+            de tarefas passada como guia.
+            */
+        }
+    }
+}
